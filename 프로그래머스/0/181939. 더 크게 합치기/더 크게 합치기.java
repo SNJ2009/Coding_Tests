38fa0972c242dd1f3879530f6964c4ea9acc2424
SNJ2009 <nagjun2009@gmail.com>
2025-11-24 11:35:35 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.11 ms, Memory: 86.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,5 @@
+class Solution {
+    public int solution(int a, int b) {
+        return Math.max(a * (int)(Math.pow(10, String.valueOf(b).length())) + b, b * (int)(Math.pow(10, String.valueOf(a).length())) + a);
+    }
+}
