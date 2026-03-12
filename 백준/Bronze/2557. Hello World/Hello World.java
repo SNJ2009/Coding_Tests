f2541f17ec8c7335cd7eddf5f77db9b9aaaf1867
SNJ2009 <nagjun2009@gmail.com>
2026-03-12 14:43:24 +0900

    [Bronze V] Title: Hello World, Time: 96 ms, Memory: 13924 KB -BaekjoonHub

--- 백준/Bronze/2557. Hello World/Hello World.java
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -1,5 +1,47 @@
-public class Main{
+class Main{
+    static void h(){
+        print("H");
+    }
+    static void e(){
+        print("e");
+    }
+    static void l(){
+        print("l");
+    }
+    static void o(){
+        print("o");
+    }
+    static void space(){
+        print(" ");
+    }
+    static void w(){
+        print("W");
+    }
+    static void r(){
+        print("r");
+    }
+    static void d(){
+        print("d");
+    }
+    static void exclamationMark(){
+        print("!");
+    }
+    static void print(String str){
+        System.out.print(str);
+    }
+    
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        h();
+        e();
+        l();
+        l();
+        o();
+        space();
+        w();
+        o();
+        r();
+        l();
+        d();
+        exclamationMark();
     }
 }
