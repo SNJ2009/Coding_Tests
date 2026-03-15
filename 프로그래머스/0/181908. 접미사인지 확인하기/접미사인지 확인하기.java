d02a00315ec16cc1250865352a03b32641041bd9
SNJ2009 <nagjun2009@gmail.com>
2026-03-16 08:28:25 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.05 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -1,5 +1,5 @@
 class Solution {
     public int solution(String my_string, String is_suffix) {
-        return is_suffix.equals(my_string.substring(my_string.length() >= is_suffix.length() ? (my_string.length() - is_suffix.length()) : 0)) ? 1 : 0;
+        return my_string.endsWith(is_suffix) ? 1 : 0;
     }
 }
