c7b9c9ed24baa073c485a98312d24170cb4c72bc
SNJ2009 <nagjun2009@gmail.com>
2026-03-27 14:41:46 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 68.8 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -2,9 +2,7 @@ class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = {0, 0};
         for(int num : num_list){
-            if(num % 2 == 0){
-                answer[0]++;
-            } else answer[1]++;
+            answer[num % 2]++;
         }
         return answer;
     }
